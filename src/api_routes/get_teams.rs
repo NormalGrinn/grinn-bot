f19ec7b3fdb4514cbbe8b866bb7697a7ec46beb6
normalgrinn <normalgrinn@gmail.com>
2024-11-18 10:08:06 +0100

    changed API format

--- src/api_routes/get_teams.rs
+++ src/api_routes/get_teams.rs
@@ -1,7 +1,13 @@
+use serde::Serialize;
 use warp::Filter;
 use serde_json::json;
 
 use crate::database;
+
+#[derive(Debug, Serialize, Clone)]
+struct Users {
+    teamless_users: Vec<String>,
+}
 
 pub fn get_teams() -> impl Filter<Extract = (impl warp::Reply,), Error = warp::Rejection> + Clone {
         warp::path("get_teams")
@@ -11,7 +17,10 @@ pub fn get_teams() -> impl Filter<Extract = (impl warp::Reply,), Error = warp::R
                     Ok(teams) => {
                         match database::get_lonely_eligible_users() {
                             Ok(users) => {
-                                let response = (teams, users);
+                                let wrapped_users = Users {
+                                    teamless_users: users,
+                                };
+                                let response = (teams, wrapped_users);
                                 let json_reply = json!(response);
                                 Ok(warp::reply::json(&json_reply))
                             },
