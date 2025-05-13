16d11c1c47e3967466bb11ca853bf25840821e4d
normalgrinn <normalgrinn@gmail/com>
2025-05-13 13:50:04 +0200

    Fuzzy updated added

--- src/commands/anime_guessing/guess.rs
+++ src/commands/anime_guessing/guess.rs
@@ -1,5 +1,6 @@
 use crate::{Context, Error};
 use rusqlite::Result;
+use rust_fuzzy_search::fuzzy_compare;
 use serenity::futures;
 use ::serenity::futures::Stream;
 use poise::serenity_prelude as serenity;
@@ -11,8 +12,15 @@ async fn autocomplete_guess<'a>(
     ctx: Context<'_>,
     partial: &'a str,
 ) -> impl Stream<Item = String> + 'a {
-    let names = database::get_filtered_names(&partial, ctx.channel_id().get()).await;
-    futures::stream::iter(names)
+    let mut names = database::get_filtered_names(ctx.channel_id().get()).await;
+    names.sort();
+    let mut similarity_tuples: Vec<(String, f32)> = names
+        .iter()
+        .map(|s| (s.clone(), fuzzy_compare(&partial.to_lowercase(), &s.to_lowercase())))
+        .collect();
+    similarity_tuples.sort_by(|a, b| b.1.partial_cmp(&a.1).unwrap_or(std::cmp::Ordering::Equal));
+    let guesses: Vec<String> = similarity_tuples.into_iter().map(|(s, _)| s).collect();
+    futures::stream::iter(guesses)
 }
 
 #[poise::command(prefix_command, track_edits, slash_command)]
